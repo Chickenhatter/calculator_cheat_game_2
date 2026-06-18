2f711d0ed772cdaaf9b25510e5b654a4e3b31a6a
Chickenhatter <b3rr1350bra1n@gmail.com>
2026-06-18 19:48:30 +1200

    6hr 6m added deg & arc

---
+++ circle_math.py
@@ -0,0 +1,24 @@
+import math
+  
+
+l = float(input('rad(1)? '))
+radius = float(input('radius: '))
+angle = float(input('angle: '))
+if l == 1:
+  asi = (angle*radius)
+  aos = (1/2)*radius*radius*angle
+else:
+  asi = ((angle/360)*2*math.pi*radius)
+  aos = ((angle/360)*math.pi*radius*radius)
+if l == 1:
+  aot = (0.5*radius*radius*sin(angle))
+else:
+  aot = (0.5*radius*radius*sin(angle*(maths.pi/180)))
+  aosh = aos-aot
+
+
+print(asi,'arc size')
+print(aos,'area of sector')
+print(aot, 'area of triangle')
+print(aosh, 'area of shade')
+  
--- sin_solver.py
+++ sin_solver.py
@@ -1,4 +1,5 @@
 import math
+radwdaw = int(input('rad(1)? '))
 A = input('A(angle): ')
 B = input('B(angle): ')
 C = input('C(angle): ')
@@ -7,15 +8,18 @@ b = input('b(size): ')
 c = input('c(size): ')
 h = input('h(area)')
 tt = 'Not True'
-if A:
-    A = float(A)
-    A = (math.radians(A))
-if B:
-    B = float(B)
-    B = (math.radians(B))
-if C:
-    C = float(C)
-    C = (math.radians(C))
+if radwdaw == 1:
+    pass
+else:
+    if A:
+        A = float(A)
+        A = ((A)*(math.pi)/180)
+    if B:
+        B = float(B)
+        B = ((B)*(math.pi)/180)
+    if C:
+        C = float(C)
+        C = ((C)*(math.pi)/180)
 if a:
     a = float(a)
 if b:
@@ -24,7 +28,6 @@ if c:
     c = float(c)
 
 for i in range(6):
-    # a = b = c
     if A and a:
         if B and not b:
             b = (math.sin(B)*a)/(math.sin(A))
@@ -52,9 +55,6 @@ for i in range(6):
             b = (math.sin(B)*c)/(math.sin(C))
         if not B and b:
             B = math.asin(b*math.sin(C)/c)
-    
-
-    #a^2=b^2+c^2-2bccosA
     if not a and b and c and A:
         a = ((b*b+c*c-2*b*c*math.cos(A))**0.5)
     if a and not b and c and B:
@@ -67,9 +67,6 @@ for i in range(6):
         B = math.acos((c*c+a*a-b*b)/(2*c*a))
     if a and b and c and not C:
         C = math.acos((a*a+b*b-c*c)/(2*a*b))
-
-
-    # h=1/2sinabc
     if A and c and b and not h:
         h = (c*b*math.sin(A))/(2)
     if B and c and a and not h:
@@ -94,41 +91,35 @@ for i in range(6):
         a = (2*h)/(b*math.sin(C))
     if not C and a and b and h:
         C = math.asin((2*h)/(a*b))
-    
     if A and B and not C:
         C = (math.pi-A-B)
-        print(C)
     if A and not B and C:
         B = (math.pi-A-C)
     if not A and B and C:
         A = (math.pi-C-B)
-
-
-
-
-
-
 if A and B and C and a and b and c:
     if abs((A+B+C)-math.pi) < 1:
         if (a+b) > c and (a+c) > b and (c+b) > a:
             tt = 'True'
-    else:
-        print('gg')
 else:
     tt = 'Unknown'
-
-
-
-
-
-
-
-if A:
-    print('A ',math.degrees(A))
-if B:
-    print('B ',math.degrees(B))
-if C:
-    print('C ',math.degrees(C))
+if A and B and C:
+    if (A+B+C) == (2*math.pi):
+        tt = 'Not True'
+if radwdaw == 1:
+    if A:
+        print('A ',A)
+    if B:
+        print('B ',B)
+    if C:
+        print('C ',C)
+else:
+    if A:
+        print('A ',A*180/(math.pi))
+    if B:
+        print('B ',B*180/(math.pi))
+    if C:
+        print('C ',C*180/(math.pi))
 if a:
     print('a ',a)
 if b:
